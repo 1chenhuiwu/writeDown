ad2c433ba33578348e9e7944c6429602a7ac2cf6
陈会武 <wuhuang@tsign.cn>
2020-05-18 11:45:50 +0800

    @wuhuang 优化后的滚动动画代码

--- scroll/index.js
+++ scroll/index.js
@@ -1,24 +1,23 @@
 /*
  * @Author: your name
  * @Date: 2020-05-11 19:07:10
- * @LastEditTime: 2020-05-11 19:07:11
+ * @LastEditTime: 2020-05-18 11:44:51
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \documentation\scroll\index.js
- */
+ */ 
 /**
  *元素的滚动动画
  *
  * @export
  * @param {*} ele 滚动的元素
  * @param {*} value 子元素到父元素（滚动元素）的offsetTop
- * @param {*} self 组件本身这样的引用空间的对象，用来清除自己本身的定时器
- * @param {*} durantion 动画时间，默认0.5秒
+ * @param {*} durantion 动画时间，默认0.2秒
  */
 // eslint-disable-next-line import/prefer-default-export
-export function scrollAnimation(ele, value, self, durantion = 500) {
+export function scrollAnimation(ele, value, durantion = 200) {
   // 清除定时器
-  clearInterval(self.pTimer)
+  clearInterval(ele.pTimer)
   if (!ele || value === undefined) {
     throw new Error('请指定滚动元素或滚动距离')
   }
@@ -27,7 +26,7 @@ export function scrollAnimation(ele, value, self, durantion = 500) {
   const frequency = 10
 
   // 被平分的份数
-  const average = Math.round(durantion / 10) || 50
+  const average = Math.floor(durantion / frequency) || 30
 
   // 滚动步长
   const step = (value - ele.scrollTop) / average
@@ -35,12 +34,12 @@ export function scrollAnimation(ele, value, self, durantion = 500) {
   // 上一次的滚动距离  用来解决世界的scrollTop达到不了子元素的offsetTop问题
   let preScrollTop
 
-  // 理论上每次滚动后的scrollTop，为了解决下一条时，每次实际的步长就加了一半的问题，比如18 + 2 = 19 ，我也是很无奈啊，不知道为啥
+  // 理论上每次滚动后的scrollTop，为了解决下一条时，每次实际的步长就加了一半的问题，比如18 + 2 = 19 ，javaScript自己的bug
   let theroyScrollTop = ele.scrollTop
 
   const clearTimer = () => {
-    clearInterval(self.pTimer)
-    self.pTimer = null
+    clearInterval(ele.pTimer)
+    ele.pTimer = null
 
     // 解决因步长太大导致滚动位置出现大偏差问题
     ele.scrollTop = value
@@ -53,14 +52,23 @@ export function scrollAnimation(ele, value, self, durantion = 500) {
 
   // 用来解决增加一次步长不滚动的事情
   let num = 2
-  self.pTimer = setInterval(() => {
+  ele.pTimer = setInterval(() => {
     if (ele.scrollTop === value) {
       clearTimer()
+
+      // JavaScript对小数运算会先转成二进制，运算完毕再转回十进制，过程中会有丢失，不过不是所有的小数间运算会有这个问题。比如： 0.1 + 0.2 = 0.30000000000000004
+      // 举例：目的值101, 100 + 1算成100.999或101.1111那么101 < (100.999 || 101.111) < 102
+    } else if (ele.scrollTop - value > 0 && ele.scrollTop - value < step) {
+      clearTimer()
+
+      // 举例：目的值101, 102 - 1算成101.111或100.999那么101 < (100.999 || 101.111) < 102
     } else if (ele.scrollTop - value < 0 && ele.scrollTop - value > step) {
       clearTimer()
-    } else if (ele.scrollTop - value < 0 && ele.scrollTop - value > step) {
-      clearTimer()
+
+      // 上次滚动长度 = 当前滚动长度，说明已经不能再滚动了
+      // 另外这个判断还可以兼容切出页面的情况
     } else if (preScrollTop === ele.scrollTop) {
+      // javaScript自己的bug增加scrollTop，不滚动
       ;(num -= 1) === 0 ? clearTimer() : addStep()
     } else {
       addStep()
